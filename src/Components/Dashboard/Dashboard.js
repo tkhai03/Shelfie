1e41d2883bd9b66b2709a7e9d55c83892dd4cce9
Thang Khai <tnkhai94@gmail.com>
2020-09-04 09:53:07 -0600

    components added

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+export default class Dashboard extends Component{
+    constructor(){
+        super()
+    }
+
+
+
+
+
+    render(){
+        return(
+            <div>
+                Dashboard.js
+                <Product/>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+export default class Product extends Component{
+    constructor(){
+        super()
+    }
+
+
+
+
+
+    render(){
+        return(
+            <div>
+                Product.js
+            </div>
+        )
+    }
+}

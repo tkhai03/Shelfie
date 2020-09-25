7216f89f9c266a2d7997ad17fdef2381fe0a5c24
Thang Khai <tnkhai94@gmail.com>
2020-09-25 12:02:04 -0600

    product css

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,10 +6,14 @@ const Product = (props) => {
 
     console.log(props)
     return (
-        <div>
-            {props.product.name}
-            <button onClick={() => props.setThisProduct(props.product)}>EDIT</button>
-            <button onClick={() => props.deleteProduct(props.product.id)}>DELETE</button>
+        <div className="product-box">
+            <img className= "product-imgurl" src={props.product.imgurl} alt={`${props.product.name}`}  />
+            <p className="product-text">{props.product.name}</p>
+            <p className="product-text">{`$${props.product.price}`}</p>
+            <div className="button-container">
+                <button className="product-button" onClick={() => props.deleteProduct(props.product.id)}>DELETE</button>
+                <button className="product-button" onClick={() => props.setThisProduct(props.product)}>EDIT</button>
+            </div>
 
         </div>
     )
